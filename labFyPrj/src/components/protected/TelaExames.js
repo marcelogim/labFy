5a50341578b88e2b4015952eef82d465ecd1b4b2
marcelogim <marcelogim@gmail.com>
2017-09-05 15:09:46 -0300

    commit

---
+++ labFyPrj/src/components/protected/TelaExames.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class TextFieldComponent extends React.Component {
+ 
+  render () {
+    return (
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+		Ola sou a tela de exames.
+			Ola sou a tela de exames.
+	  </div>
+	  
+	
+    )
+  }
+}
